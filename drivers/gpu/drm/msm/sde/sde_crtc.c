23f3bbd22738e2a2a3a6c962cd4173b211805d05
Dan Cartier <nospamdan1@gmail.com>
2020-09-09 10:01:02 -0400

    Revert "drm: allow dc dimming code to run"
    This reverts commit b7a0d5fa891246ea6552b1f3ca0aca36f134bf19.
    
    Change-Id: I47a738a42e4e3378a53bc96d49d38d61d17b6abc

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -5780,10 +5780,17 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
         if (mode ==3)
             aod_index = i;
 	}
-	if(fp_index >=0 && dim_mode!=0)
+
+	if (fp_mode == 1) {
 		display->panel->dim_status = true;
-	else
+		cstate->fingerprint_pressed = true;
+		return 0;
+	} else {
 		display->panel->dim_status = false;
+		cstate->fingerprint_pressed = false;
+		cstate->fingerprint_dim_layer = NULL;
+		return 0;
+	}
 
 	if (fppressed_index > 0 || fp_mode == 1) {
 		cpu_input_boost_kick_max(500);
